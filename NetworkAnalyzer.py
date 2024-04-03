cd3fac000bc5a1e29f34656bb9d1edd3fa3ef20d
alancasre133 <72096324+alancasre133@users.noreply.github.com>
2024-04-03 03:28:28 -0600

    TCP layer metadata

--- NetworkAnalyzer.py
+++ NetworkAnalyzer.py
@@ -2,7 +2,11 @@ from collections import defaultdict
 import pandas as pd
 from scapy.all import *
 total=0
-
+def hasTCP(packet: Packet):
+    if packet.haslayer("TCP"):
+        return packet.getlayer("TCP").fields
+    else:
+        return None
 def hasIPLayer(packet: Packet):
     if packet.haslayer("IP"):
         return packet.getlayer("IP").fields
@@ -10,10 +14,33 @@ def hasIPLayer(packet: Packet):
         return packet.getlayer("IPv6").fields
     else:
         return None
+def getProtocolNameWithNumberOfProtocol(protocol: int):
+    nameofprotocol={0:"hopot",6:"tcp_ip",17:"udp_ip",2:"igmp_ip",58:"icmp_ip"}
+    return nameofprotocol[protocol] or None
 def packet_callback(packet):
+    metadatainpacketfordictionarie={}
+    #print(packet.layers)
     IPLayer=hasIPLayer(packet)
-    if IPLayer and IPLayer["version"]==4:
-        print(IPLayer)
+    if IPLayer:
+        metadatainpacketfordictionarie["source"]=IPLayer["src"]
+        metadatainpacketfordictionarie["destination"]=IPLayer["dst"]
+        try:
+            metadatainpacketfordictionarie["protocolName"]=getProtocolNameWithNumberOfProtocol(IPLayer["proto"])
+        except:
+            try:
+                metadatainpacketfordictionarie["protocolName"]=getProtocolNameWithNumberOfProtocol(IPLayer["nh"])
+            except:
+                print(IPLayer)
+    else:
+        metadatainpacketfordictionarie["source"]=0
+        metadatainpacketfordictionarie["destination"]=0
+        metadatainpacketfordictionarie["protocolName"]=0
+    TCPLayer = hasTCP(packet)
+    if TCPLayer:
+        print(TCPLayer)
+    #print(metadatainpacketfordictionarie)
+    
+
 show_interfaces()
 
-sniff( prn=packet_callback)
+sniff( prn=packet_callback,count=150)
