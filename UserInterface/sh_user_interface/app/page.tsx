d381c2ce6abfe4ae4215050c94c3a5c821d3262c
Misael Cabrera <a327822@alumnos.uaslp.mx>
2024-04-03 02:36:23 -0600

    [SH-SISA]: Test

--- UserInterface/sh_user_interface/app/page.tsx
+++ UserInterface/sh_user_interface/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Bsaday{" "}
+            Vamos a ver sisierto{" "}
           </a>
         </div>
       </div>
