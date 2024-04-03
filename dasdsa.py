1999a6c203e5c3b394a317ccd0edbe129a5745aa
alancasre133 <72096324+alancasre133@users.noreply.github.com>
2024-04-03 17:59:04 -0600

    TCP layer metadata

--- dasdsa.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-
-def packet_callback(packet):
-    print(" ---  Paquete capturado  ---")
-    print(packet.show())
-    
-show_interfaces()
-sniff(prn=packet_callback,iface="Software Loopback Interface 1")
